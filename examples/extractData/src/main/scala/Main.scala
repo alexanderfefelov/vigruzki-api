dc2a599847a36ccf5c7a9d238e3d016f22dac303
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-03-01 19:37:39 +0300

    Fix typo

--- examples/extractData/src/main/scala/Main.scala
+++ examples/extractData/src/main/scala/Main.scala
@@ -15,7 +15,7 @@ object Main extends App {
   val rawXml = XML.loadFile(dumpXml)
   val register = scalaxb.fromXML[RegisterType](rawXml)
   File("urls-unique.txt").write(register.content.flatMap(_.url).distinct.mkString("\n"))
-  File("domains- unique.txt").write(register.content.flatMap(_.domain).filterNot(_.contains("*")).distinct.mkString("\n"))
+  File("domains-unique.txt").write(register.content.flatMap(_.domain).filterNot(_.contains("*")).distinct.mkString("\n"))
   File("domain-masks-unique.txt").write(register.content.flatMap(_.domain).filter(_.contains("*")).distinct.mkString("\n"))
   File("ips-unique.txt").write(register.content.flatMap(_.ip).distinct.mkString("\n"))
   File("ip-subnets-unique.txt").write(register.content.flatMap(_.ipSubnet).distinct.mkString("\n"))
