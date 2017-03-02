4cf7d6fa5ca15b14ca8d362334dd486a2ae48ba0
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-03-02 09:09:59 +0300

    Extract more data

--- examples/extractData/build.sbt
+++ examples/extractData/build.sbt
@@ -3,5 +3,6 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.scala-lang.modules" %% "scala-xml" % "1.0.6",
   "alexanderfefelov.github.com" %% "vigruzki-api" % "3.1",
-  "com.github.pathikrit" %% "better-files" % "2.16.0"
+  "com.github.pathikrit" %% "better-files" % "2.16.0",
+  "com.netaporter" %% "scala-uri" % "0.4.16"
 )
--- examples/extractData/src/main/scala/Main.scala
+++ examples/extractData/src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 import better.files.File
-
-import xml._
+import com.netaporter.uri._
+import com.netaporter.uri.config.UriConfig
+import com.netaporter.uri.decoding.NoopDecoder
 import ru.gov.rkn.vigruzki._
+
+import scala.xml._
 
 object Main extends App {
 
@@ -14,10 +17,22 @@ object Main extends App {
 
   val rawXml = XML.loadFile(dumpXml)
   val register = scalaxb.fromXML[RegisterType](rawXml)
-  File("urls-unique.txt").write(register.content.flatMap(_.url).distinct.mkString("\n"))
-  File("domains-unique.txt").write(register.content.flatMap(_.domain).filterNot(_.contains("*")).distinct.mkString("\n"))
-  File("domain-masks-unique.txt").write(register.content.flatMap(_.domain).filter(_.contains("*")).distinct.mkString("\n"))
-  File("ips-unique.txt").write(register.content.flatMap(_.ip).distinct.mkString("\n"))
-  File("ip-subnets-unique.txt").write(register.content.flatMap(_.ipSubnet).distinct.mkString("\n"))
+
+  val urlsUnique = register.content.flatMap(_.url).distinct
+  val domainsUnique = register.content.flatMap(_.domain).filterNot(_.contains("*")).distinct
+  val domainMasksUnique = register.content.flatMap(_.domain).filter(_.contains("*")).distinct
+  val ipsUnique = register.content.flatMap(_.ip).distinct
+  val ipSubnetsUnique = register.content.flatMap(_.ipSubnet).distinct
+  implicit val uriConfig = UriConfig(decoder = NoopDecoder)
+  val domainsFromUrlsUnique = urlsUnique.flatMap(Uri.parse(_).host).distinct
+  val domainsUniqueTotal = (domainsUnique ++ domainsFromUrlsUnique).distinct
+
+  File("urls-unique.txt").write(urlsUnique.mkString("\n"))
+  File("domains-unique.txt").write(domainsUnique.mkString("\n"))
+  File("domain-masks-unique.txt").write(domainMasksUnique.mkString("\n"))
+  File("ips-unique.txt").write(ipsUnique.mkString("\n"))
+  File("ip-subnets-unique.txt").write(ipSubnetsUnique.mkString("\n"))
+  File("domains-from-urls-unique.txt").write(domainsFromUrlsUnique.mkString("\n"))
+  File("domains-unique-total.txt").write(domainsUniqueTotal.mkString("\n"))
 
 }
