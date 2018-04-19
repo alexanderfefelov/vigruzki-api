68f80800fad21ade63bd7d71e324844eaa89c2d3
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-04-19 05:19:08 +0300

    Refactor WHOIS resolver

--- examples/extractData/ip-subnets-unique-whois.py
+++ examples/extractData/ip-subnets-unique-whois.py
@@ -5,6 +5,7 @@ import ipaddr
 from ipwhois import IPWhois
 import os
 
+
 def main():
     home = os.path.dirname(os.path.abspath(__file__))
     os.chdir(home)
@@ -12,6 +13,8 @@ def main():
     with io.open("ip-subnets-unique.txt") as in_file:
         lines = in_file.readlines()
         with io.open("ip-subnets-unique-whois.txt", "w") as out_file:
+            out_file.write(u"# Attention! All host counters include broadcast and network addresses.\n\n")
+            total_hosts = 0
             for line in lines:
                 src = line.strip()
                 print src
@@ -20,9 +23,10 @@ def main():
                 whois_data = whois.lookup_rdap(depth = 1)
                 network_name = whois_data["network"]["name"]
                 hosts = ipaddr.IPv4Network(src).numhosts
-                out_file.write("# " + network_name + "\n")
-                out_file.write(u"# " + str(hosts) + " host(s)\n")
-                out_file.write(src + "\n\n")
+                total_hosts += hosts
+                out_file.write(u"# {0}\n# {1} host(s)\n{2}\n\n".format(network_name, hosts, src))
+            out_file.write(u"# {0} host(s)\n".format(total_hosts))
+
 
 if __name__ == '__main__':
     main()
